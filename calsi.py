cd13c4639851d28e298e03245d3d5dc519145c52
veleswari <veleswaripaluru@gmail.com>
2021-06-27 14:58:06 +0530

    task

---
+++ calsi.py
@@ -0,0 +1,15 @@
+def adding(a,b):
+    c = a+b
+    return c
+
+def subtract(a,b):
+    k = a - b
+    return k
+
+def mutipl(a,b):
+    l = a * b
+    return l
+
+def division(a,b):
+    m = a/b
+    return m
